581aa332b8c1c03a3934bf505fbf2a087a152d4a
Nikita Repin <88780189+repinikita@users.noreply.github.com>
2023-11-30 16:18:58 +0500

    Update HW_3.sql

--- HW_3.sql
+++ HW_3.sql
@@ -1,35 +1,35 @@
 -- Вложенные Запросы
 -- 1.Покажите название и длительность в секундах самой длинной песни применив округление по правилам математики. Столбец назвать sec.
-select Name, round(Milliseconds/1000) as sec
- from Chinook.Track
-where Milliseconds = (select max(Milliseconds) from chinook.track);
+SELECT Name, round(Milliseconds/1000) AS sec
+FROM Chinook.Track
+WHERE Milliseconds = (SELECT max(Milliseconds) FROM chinook.track);
 
 -- 2.Покажите все счёт-фактуры, стоимость которых ниже средней.
-select * from Chinook.invoice
-where total < (select avg(total) from Chinook.invoice);
+SELECT * FROM Chinook.invoice
+WHERE total < (SELECT avg(total) FROM Chinook.invoice);
 
 -- 3.Покажите счёт-фактуру с высокой стоимостью.
-select * from chinook.invoice 
-where total = (select max(total) from chinook.invoice );
+SELECT * FROM chinook.invoice 
+WHERE total = (SELECT max(total) FROM chinook.invoice );
 
 -- 4.Покажите города, в которых живут и сотрудники, и клиенты .
-select city  from chinook.customer 
-where city IN (select city from chinook.employee );
+SELECT city  FROM chinook.customer 
+WHERE city IN (SELECT city FROM chinook.employee );
 
 -- 5.Покажите имя, фамилию покупателя (номер 19), компанию и общую сумму его заказов. Столбец назовите sum.
-select FirstName, LastName, Company, (select sum(total) from Chinook.Invoice where customerid = '19' ) as sum  from Chinook.Customer
-where customerId = '19';
+SELECT FirstName, LastName, Company, (SELECT sum(total) FROM Chinook.Invoice WHERE customerid = '19' ) AS sum  FROM Chinook.Customer
+WHERE customerId = '19';
 
 -- 6.Покажите сколько раз покупали треки композитора группы Queen.  Количество покупок необходимо посчитать по каждому треку. 
 -- Вывести название, ИД трека и количество купленных треков композитора группы Queen. Столбец назовите total.
 
-select Name, trackId, (select count(Chinook.InvoiceLine.TrackId) from Chinook.InvoiceLine where Chinook.InvoiceLine.TrackId = Chinook.Track.TrackId ) as total 
-from chinook.track
-where composer = 'Queen';
+SELECT Name, trackId, (SELECT COUNT(Chinook.InvoiceLine.TrackId) FROM Chinook.InvoiceLine WHERE Chinook.InvoiceLine.TrackId = Chinook.Track.TrackId ) AS total 
+FROM chinook.track
+WHERE composer = 'Queen';
 
 -- 7. Посчитайте колличество треков в каждом альбоме.В результате вывести имя альбома и кол-во треков.
-select title , (select count(*) from chinook.track where track.albumid = album.albumId ) as count
-from chinook.album;
+SELECT title , (SELECT COUNT(*) FROM chinook.track WHERE track.albumid = album.albumId ) AS COUNT
+FROM chinook.album;
 
 
 
