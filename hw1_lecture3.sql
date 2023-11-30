a5b3d91c4106171f7169376114dd335bac2056e5
Nikita Repin <88780189+repinikita@users.noreply.github.com>
2023-11-30 16:10:45 +0500

    Update hw1_lecture3.sql

--- hw1_lecture3.sql
+++ hw1_lecture3.sql
@@ -1,48 +1,48 @@
 -- HW
 -- Покажите содержимое таблицы исполнителей (артистов)
-select * from Chinook.artist;
+SELECT * FROM Chinook.artist;
 
 -- Покажите фамилии и имена клиентов из города Лондон
-select * from Chinook.Customer where city = "London";
+SELECT * FROM Chinook.Customer WHERE city = "London";
 
 -- Покажите продажи за 2012 год, со стоимостью продаж более 4 долларов
-select * from Chinook.invoice
-where invoicedate between '2012-01-01' and '2012-12-31' and total > 4;
+SELECT * FROM Chinook.invoice
+WHERE invoicedate BETWEEN '2012-01-01' AND '2012-12-31' AND total > 4;
 
 -- Покажите дату продажи, адрес продажи, город в которую совершена продажа и стоимость покупки равную 8.91.
 -- Присвоить названия столбцам InvoiceDate – Дата_Продажи, BillingAddress – Адрес_Продажи и BillingCity - Город_Продажи.
-select InvoiceDate as Дата_Продажи ,BillingAddress as Адрес_Продажи  ,BillingCity as Город_Продажи , total
-from Chinook.Invoice
-where total = 8.91;
+SELECT InvoiceDate AS Дата_Продажи ,BillingAddress AS Адрес_Продажи  ,BillingCity AS Город_Продажи , total
+FROM Chinook.Invoice
+WHERE total = 8.91;
 
 -- Покажите фамилии и имена сотрудников компании, нанятых в 2002 году и проживающих в городе Edmonton
-select LastName,FirstName, HireDate
-from Chinook.Employee
-where HireDate between '2002-01-01 00:00:00' and '2002-12-31 00:00:00' and city = 'Edmonton';
+SELECT LastName,FirstName, HireDate
+FROM Chinook.Employee
+WHERE HireDate BETWEEN '2002-01-01 00:00:00' AND '2002-12-31 00:00:00' AND city = 'Edmonton';
 
 -- Покажите канадские города, в которые были сделаны продажи в августе.
 
-select BillingCity, InvoiceDate
-from Chinook.Invoice
-where billingcountry = 'Canada' and InvoiceDate like '%-08-%';
+SELECT BillingCity, InvoiceDate
+FROM Chinook.Invoice
+WHERE billingcountry = 'Canada' AND InvoiceDate LIKE '%-08-%';
 
 -- Покажите Фамилии и имена работников, нанятых в мае. Решите 2-мя способами:
 -- используя оператор like
-select LastName,FirstName,HireDate
-from Chinook.Employee
-where HireDate like '%-05-%';
+SELECT LastName,FirstName,HireDate
+FROM Chinook.Employee
+WHERE HireDate LIKE '%-05-%';
 
 -- используя форматирование даты*. 
-select LastName,FirstName,  DATE_FORMAT( hiredate , ' %M %Y') as DateFormated
-from Chinook.Employee
+SELECT LastName,FirstName,  DATE_FORMAT( hiredate , ' %M %Y') AS DateFormated
+FROM Chinook.Employee
 WHERE MONTH(HireDate) = 5 ;
 
 -- Покажите фамилии и имена сотрудников, занимающих должность менеджера по продажам и ИТ менеджера. Решите задание двумя способами: 
 -- используя оператор IN
-select LastName,FirstName,Title
-from Chinook.Employee
-where title In ( 'Sales Manager', 'It Manager');
+SELECT LastName,FirstName,Title
+FROM Chinook.Employee
+WHERE title IN ( 'Sales Manager', 'It Manager');
 -- используя логические операции
-select LastName,FirstName,Title
-from Chinook.Employee
-where title = 'Sales Manager' or title =  'It Manager';
+SELECT LastName,FirstName,Title
+FROM Chinook.Employee
+WHERE title = 'Sales Manager' OR title =  'It Manager';
